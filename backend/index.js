d7587a6405d53f90866462bd1d706c78e875fd92
AyushRajSinghParihar <1bi22ri009@bit-bangalore.edu.in>
2024-09-30 05:24:23 +0530

    added routes to user.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
 const express = require("express");
+const mainRouter = require("./routes/index")
+const userRouter = require("./routes/user")
+const app = express();
 
-
+app.use("/api/v1", mainRouter);
+app.use("/api/v1/user", userRouter)
