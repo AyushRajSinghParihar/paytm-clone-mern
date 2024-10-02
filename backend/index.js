2d8d40d4d81d245656b7ecad6796ab3acf64f593
AyushRajSinghParihar <1bi22ri009@bit-bangalore.edu.in>
2024-10-02 12:18:41 +0530

    added cors jwt

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
 const mainRouter = require("./routes/index")
-const userRouter = require("./routes/user")
 const app = express();
 
 app.use("/api/v1", mainRouter);
-app.use("/api/v1/user", userRouter)
+
+app.listen(3000);
