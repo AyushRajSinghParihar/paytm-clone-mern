61c19d85efc82f4c2b7f301795d7edf41b2ae5dd
AyushRajSinghParihar <1bi22ri009@bit-bangalore.edu.in>
2024-10-02 14:44:34 +0530

    signup route defined

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const zod = require("zod");
+const { User } = require('./db');
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 
-router.get("/", (req,res) => {
-    
+
+const signupSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+
+router.post("/signup", async (req,res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+    if(!success) {
+        return res.json({
+            message:"Email already taken/Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username:body.username 
+    })
+
+    if(user._id) {
+        return res.status(411).json({
+            message: "Email already taken/ Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body);
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, JWT_SECRET)
+
+    res.status(201).json({
+        message: "User created successfully",
+        token: token
+    })
+
 })
 
 module.exports = router;
